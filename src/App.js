c5d0f95d44e8e9aa41e401e8730daf359e0a9945
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-09-01 10:50:49 -0400

    Box generator with sensei bonus of size

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, {useState} from 'react';
 import './App.css';
+import Form from './components/Form'
+import Box from './components/Box'
 
 function App() {
+
+  const [boxes, setBoxes] = useState([]);
+
+  const [boxDetails, setBoxDetails] = useState({
+    color: "",
+    size: "",
+  })
+
   return (
     <div className="App">
-      <legend>App.js</legend>
+      <legend>Form.jsx</legend>
       <fieldset>
-        <BoxColorForm></BoxColorForm>
-        <Boxes></Boxes>
+        <Form
+          boxDetails={boxDetails}
+          setBoxDetails={setBoxDetails}
+          boxes = {boxes}
+          setBoxes={setBoxes}
+        />
       </fieldset>
+      <legend>Box.jsx</legend>  
+      <fieldset>
+        {
+          boxes.map(box => <Box color={box.color} size={box.size}/>)
+        }
+      </fieldset>
+
     </div>
   );
 }
